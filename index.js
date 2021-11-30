f78fe46322238e838522a0137a558122b0d062b3
Krishna Dhingra <dkrishna1608@gmail.com>
2021-12-01 03:43:01 +0530

    Now logs will be shown

--- index.js
+++ index.js
@@ -15,14 +15,43 @@ mongoose.connect("mongodb://localhost:27017/blog")
 
 app.get("/users", (req, res) => {
 
-    // this function will display all the users
-    getData()
-    async function getData(){
-        const data = await User.find()
-        res.json(data)
+    try{
+
+        // this function will display all the users
+        getData()
+        async function getData(){
+            const data = await User.find()
+            res.json(data)
+    
+            const log = await Logs.create({
+                message: `List of all the users were shown at ${Date()}`
+            })
+        }
+
+    } catch(e){
+        console.log(e.message)
     }
 })
 
+app.get("/logs", (req, res) => {
+
+    try{
+
+        // this function will display all the logs
+        getData()
+        async function getData(){
+            const data = await Logs.find()
+            res.json(data)
+    
+            const log = await Logs.create({
+                message: `List of all the logs were shown at ${Date()}`
+            })
+        }
+
+    } catch(e){
+        console.log(e.message)
+    }
+})
 
 
 // This will create a new user
@@ -31,7 +60,7 @@ app.post("/users", (req, res) => {
     // checking if username, email and password is provided if not it will display an error message
     if(req.body.username && req.body.email && req.body.password){
 
-        createUser(req.body.username, req.body.age ? req.body.age : null, req.body.email, req.body.password)
+        createUser(req.body.username, req.body.email, req.body.password)
 
         res.json({msg: "New user created successfully"})
     }else{
@@ -47,7 +76,9 @@ app.put("/users", (req, res) => {
         
         getUser()
         async function getUser(){
-            
+
+            // username and password is mandatory but it's also necessary to either pass new email, new username or new password
+
             if(req.body.username && req.body.password && req.body.newEmail || req.body.newUsername || req.body.newPassword){
                 
                 const data = await User.where("username").equals(req.body.username)
@@ -81,15 +112,22 @@ app.put("/users", (req, res) => {
 
 
 // this function will create a new user
-async function createUser(username, userAge, email, password){
+async function createUser(username, email, password){
     try{
         const user = await User.create({
             username: username,
-            age: userAge ? userAge : null,
             email: email,
             password: password,
         })
-        console.log(user)
+
+        const log = await Logs.create({
+            message: "A new user was created with the following credentials",
+            username: username,
+            password: password,
+            email: email,
+        })
+    
+        console.log(log)
 
     } catch(e){
         console.log(e.message)
@@ -111,15 +149,22 @@ async function updateUser(id, preUsername, preEmail, prePassword, Username, Pass
         const data = await User.where("_id").equals(id).updateMany({"username": username, "email": email, "password": password})
     }
 
-    // const log = await Logs.create({
-    //     username: username,
-    //     password: password,
-    //     newEmail: newEmail ? newEmail : 'same',
-    //     newUsername: newUsername ? newUsername : 'same',
-    //     newPassword: newPassword ? newPassword : 'same'
-    // })
+    // making a changing list that will tell that what all was changed when the user updated it's info
+    changeList = []
+    username === preUsername ? null : changeList.push("username")
+    email === preEmail ? null : changeList.push("email")
+    password === prePassword ? null : changeList.push("password")
 
-    // console.log(log)
+    const log = await Logs.create({
+        message: `The user changed ${changeList} at ${Date()}`,
+        username: username,
+        password: password,
+        newEmail: email,
+        newUsername: username,
+        newPassword: password
+    })
+
+    console.log(log)
 }
 
 app.listen(PORT, () => {
--- schema.js
+++ schema.js
@@ -14,30 +14,21 @@ try{
         password: {
             type: String,
             required: true
-        }
+        },
+        createdAt: String
     })
     
-    const updateUserSchema = new mongoose.Schema({
-        username: {
-            type: String,
-            required: true
-        },
-        password: {
-            type: String,
-            required: true
-        },
+    const LogsSchema = new mongoose.Schema({
+        message: String,
+        username: String,
+        password: String,
         newEmail: String,
         newUsername: String,
-        newPassword: String,
-        updatedAt: {
-            type: Date,
-            default: () => Date.now,
-            immutable: true
-        }
+        newPassword: String
     })
     
     module.exports.User = mongoose.model("users", newUserSchema)
-    module.exports.Lo = mongoose.model("logs", updateUserSchema)
+    module.exports.Logs = mongoose.model("logs", LogsSchema)
 
 } catch(e){
     console.log(e.message)
